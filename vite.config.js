dedafb0a4647b26d4d38bdab5dbbb8ee802356ea
heuderdev <heuderdev@gmail.com>
2025-11-05 01:15:30 -0300

    feat: config base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/clinic/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(), 
